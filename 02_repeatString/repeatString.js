963910f8b07c7d7e3a727d0e0a0704c9998d6c96
Don <dagem.alemseged1@gmail.com>
2022-10-14 11:12:22 -0400

    Write code to repeat a string any number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringXnum = '';
+    if (num < 0) return 'ERROR';
+    for (let i = 0; i < num; i++) {
+        stringXnum += string;
+    }
+    return stringXnum;
 };
 
 // Do not edit below this line
