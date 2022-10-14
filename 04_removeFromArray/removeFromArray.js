551f5dfc6fc8464c62bbc5281f939a536bfb56d2
Don <dagem.alemseged1@gmail.com>
2022-10-14 13:17:46 -0400

    Write code that removes cetain elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+    let newArr = arr.filter(element => !num.includes(element));
+    return newArr;
 };
 
 // Do not edit below this line
