46bf011e94d94a12c5978f8fbb71af4a4fa3da74
Don <dagem.alemseged1@gmail.com>
2022-10-15 10:51:50 -0400

    Write code to find sum between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof start != "number" || typeof end != "number"  || start < 0 || end < 0) return 'ERROR';
+    if (end < start) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
