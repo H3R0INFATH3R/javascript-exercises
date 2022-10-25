c749300b32b6c663eeb24a4085e81998e87c86d3
Don <dagem.alemseged1@gmail.com>
2022-10-25 04:59:34 -0400

    Write code to find the fibonnacci of a number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return 'OOPS';
 
+    num = Number(num);
+
+    let a = 0, b = 1, c = num;
+    for (let i = 2; i <= num; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
