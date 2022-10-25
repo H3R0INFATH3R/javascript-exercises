d2452dc82a7eabb21691ac3432e65a9da890c7e6
Don <dagem.alemseged1@gmail.com>
2022-10-25 04:24:15 -0400

    Write code to check if a given string is palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace('.', '');
+    string = string.replace('!', '');
+    string = string.replace(/,/g, '');
+    string = string.replace(/ /g, '');
+    let str = string.split('');
+    str = str.reverse();
+    str = str.join('');
 
+    return string === str ? true : false;
 };
 
 // Do not edit below this line
