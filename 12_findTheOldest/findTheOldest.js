f6adc7d05bb09d0d7c7c8b3647b02f4af756f564
Don <dagem.alemseged1@gmail.com>
2022-10-30 06:54:33 -0400

    Write code to find the oldest person from an array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let newArr = arr.map(function(element) {
+        if (!Object.hasOwn(element, 'yearOfDeath')) {
+            let date = new Date().getFullYear();
+            element.yearOfDeath = date;
+        }
+        return element.yearOfDeath - element.yearOfBirth;
+    });
+    
+    let max = newArr[0];
+    let index = 0;
+    for (let i = 1; i < newArr.length; i++) {
+        if (newArr[i] > max) {
+            max = newArr[i];
+            index = i;
+        }
+    }
+    return arr[index];
 };
 
 // Do not edit below this line
